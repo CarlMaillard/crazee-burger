ec2d7738493abd3760411b77fcf789b6d2eb0458
CarlMaillard <59692132+CarlMaillard@users.noreply.github.com>
2024-11-25 17:37:47 +0100

    app.js > Add my first name

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          oui <code>src/App.jsx</code> and save to test HMR
+          Hello Carlito <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
