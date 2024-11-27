4bddc6c4c111e0ccb264083dbfeb9c68b03bfb7a
CarlMaillard <59692132+CarlMaillard@users.noreply.github.com>
2024-11-27 17:54:27 +0100

    ✨ F02: Add routing

---
+++ src/components/pages/error/ErrorPage.jsx
@@ -0,0 +1,10 @@
+export default function ErrorPage() {
+    //state 
+
+    //comportements
+
+    //affichage (render)
+  return (
+    <div>ErrorPage</div>
+  )
+}
---
+++ src/components/pages/order/OrderPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function OrderPage () {
+    //state 
+
+    //comportements
+
+    //affichage (render)
+  return    (
+    <h1>Order page !</h1>
+    
+  )          
+}
