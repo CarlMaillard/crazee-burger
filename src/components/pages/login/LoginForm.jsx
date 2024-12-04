fbd505b990f568befa3cb68da7ee1b1fa1101669
CarlMaillard <59692132+CarlMaillard@users.noreply.github.com>
2024-12-04 11:15:21 +0100

    💄 install Styled components &  add theme file index.js

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
-
+import styled from 'styled-components';
 
 export default function LoginForm() {
  //state
@@ -23,12 +23,24 @@ export default function LoginForm() {
   return (
         //affichage (render)
 
-    <form action="submit" onSubmit={handleSubmit}>
+    <LoginFormStyled action="submit" onSubmit={handleSubmit}>
     <h1>Bienvenue chez nous !</h1>
     <br />
     <h2>Connectez-vous </h2>
         <input value={inputValue}  type="text" placeholder='Entrez votre prénom...' onChange={handleChange} required/>
         <button> Accéder à votre espace</button>
-    </form>     
+    </LoginFormStyled>     
   )
 }
+
+
+const LoginFormStyled = styled.form`
+border: 10px  solid red;
+ `
+/** 4 methodes pour ajouter du style à un composant.
+ * 1. inline style
+ * 2. object style
+ * 3. modules css(avec classname)
+ * 4. global style (index.css)
+ * 5. style component
+ */
