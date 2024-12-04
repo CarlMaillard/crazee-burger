dfa78b2c9aabed55c520f36b0b7010c2ff6604de
CarlMaillard <59692132+CarlMaillard@users.noreply.github.com>
2024-12-04 17:08:01 +0100

    💄 F03: LOGIN > Add styling and background img

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import LoginForm from './LoginForm'
 import styled from 'styled-components';
-import burgerBackground from '../../../assets/images/F03-burger-background.jpg';
 import Logo from '../../reusable-ui/Logo';
 
 export default function LoginPage () {
@@ -19,15 +18,25 @@ export default function LoginPage () {
 }
 
 const LoginPageStyled = styled.div`
-  background: red;
   height: 100vh;
-  overflow: hidden;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-image: url(${burgerBackground});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
+
+  ::before{
+    content: "";
+    background: url("./images/F03-burger-background.jpg") rgb(0, 0, 0, 0.7);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-blend-mode: darken;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1; 
+  }
 `;
