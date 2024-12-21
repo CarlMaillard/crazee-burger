ef5b65c9f8dddf5dc4b367bf0eab0a9abd04bc86
CarlMaillard <59692132+CarlMaillard@users.noreply.github.com>
2024-12-21 16:14:17 +0100

    💄 F05: ORDER > Product > add image and infos-texte layout

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -31,47 +31,4 @@ const MenuStyled = styled.div`
    padding: 50px 50px 150px;
    justify-items: center;
 
-    .produit {
-      background: ${theme.colors.background_white};
-      height: 330px;
-      width: 240px;
-      color: black;
-      display: flex;
-      flex-direction: column;
-      align-items: left;
-      justify-content: space-between;
-      border-radius: ${theme.borderRadius.extraRound};
-      margin: auto;
-      padding: 20px;
-      padding-top: 50px;
-      padding-bottom: 14px;
-      box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
-
-      .image{
-        border: 1px solid fuchsia;
-        width: 100px;
-        height: auto;
-        img {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-          /* background-size: cover;
-          background-position: center;
-          background-repeat: no-repeat; */
-        }
-      }
-
-      .food-name {
-        background: yellow;
-        color: black;
-        font-size: ${theme.fonts.sizes.P4};
-      }
-
-      .item-bottom-block{
-        background: orange;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;      
-      }
-    }
 `;
--- src/components/pages/order/Main/Product.jsx
+++ src/components/pages/order/Main/Product.jsx
@@ -1,51 +1,70 @@
-// import styled from "styled-components";
-// import { theme } from "../../../../theme";
+import styled from "styled-components";
+import { theme } from "../../../../theme";
 
 export default function Product({title, imageSource, price}) {
   return (
-   <div className="produit">
+   <ProductStyled className="produit">
         <div className="image">
             <img src={imageSource} alt={title}></img>
         </div>
         <div className="info-text">
-        <div className="description">
-            <div className="title">{title}</div>
+          <div className="title">{title}</div>
+          <div className="description">
             <div className="price">{price}</div>
             <button className="add-button">Ajouter</button>
+          </div>
         </div>
-        </div>
-    </div> 
+    </ProductStyled> 
   )
 }
 
-const ProduitStyled = styled.div`
+const ProductStyled = styled.div`
     background: ${theme.colors.background_white};
-    height: 330px;
-    width: 240px;
+    height: 300px;
+    width: 200px;
     color: black;
-    display: flex;
-    flex-direction: column;
-    align-items: left;
-    justify-content: space-between;
+    display: grid;
+    grid-template-rows: 65% 1fr;
+    grid-row-gap: 10px;
+    justify-items: center;
+
+
     border-radius: ${theme.borderRadius.extraRound};
     margin: auto;
     padding: 20px;
-    padding-top: 50px;
-    padding-bottom: 14px;
+    padding-bottom: 10px;
     box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
     
     .image{
+        margin-top: 30px;
         border: 1px solid fuchsia;
-        width: 100px;
+        width: 100%;
         height: auto;
         img {
           width: 100%;
           height: 100%;
-          object-fit: cover;
-          /* background-size: cover;
+          object-fit: contain;
+          background-size: cover;
           background-position: center;
-          background-repeat: no-repeat; */
+          background-repeat: no-repeat;
         }
       }
 
+    .info-text {
+      width: 100%;
+      justify-items: left;
+    }
+
+    .title {
+      border: 1px solid black;
+    }
+
+    .description{
+        border: 1px solid red;
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        justify-items: left;
+    }
 `;
